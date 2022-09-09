2877940589a4da2cf874b3ffa463d73d66afa123
2020073190 <chdragon01@hanyang.ac.kr>
2022-09-09 16:07:58 +0900

    2-1-3 resolve issue

--- 2-1/2-1-3/convert.cc
+++ 2-1/2-1-3/convert.cc
@@ -1,59 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
-int exiting = 1;
 
-void convert_case(char* a) {
-	unsigned int len = (unsigned int)strlen(a);
-	for (int i=0; i<len; i++)
+int ex = 1;
+
+void lower_case(char** a) 
+{
+	char str[100];
+	strcpy(str, *a);
+	int lenof_str = sizeof(str) / sizeof(char);
+
+	for (int i=0; i<lenof_str; i++)
 	{
-		if ((int)a[i] >= 97 && (int)a[i] < 123)
+		if ((int)str[i] >= 65 && (int)str[i] < 91)
 		{
-			a[i] = (char) ((int)a[i] - 32);
+			str[i] = (char) ((int)str[i] + 32);
 		}
-		else if ((int)a[i] >= 65 && (int)a[i] < 91)
+	}
+	*a = str;
+	return;
+}
+
+void convert_case(char** a, int l) 
+{
+	char str[100];
+	int i=0; 
+
+	while ((int)(*a)[i]>0)
+	{
+		int each_char = (int)(*a)[i];
+		if (each_char >= 97 && each_char < 123)
 		{
-			a[i] = (char) ((int)a[i] + 32);
+			str[i] = (char) (each_char - 32);
+		}
+		else if (each_char >= 65 && each_char < 91)
+		{
+			str[i] = (char) (each_char + 32);
 		}
 		else
 		{
 			printf("Error: Wrong input!\n");
-			exiting = 0;
+			ex = 0;
 			return;
 		}
+		i++;
 	}
+	printf("%s\n", str);
+	return;
 }
-int main(void) {
-//Implement this function
-	char init[10000];
-	char* pstr;
-
-	while (exiting)
+int main(void) 
+{
+	char init[100];
+	int lenof_init;
+	char check_exit[100];
+	char* ptr_check_exit;
+	char** pptr_check_exit;
+	while (ex)
 	{
 		scanf(" %s", init);
-		unsigned int len = (unsigned int)strlen(init);
+		lenof_init = sizeof(init)/sizeof(char);
+		strcpy(check_exit, init);
+		ptr_check_exit = check_exit;
+		pptr_check_exit = &ptr_check_exit;
 
-		pstr = (char*) malloc(sizeof(char) * len);
-		for (int i=0; i<len; ++i)
+		lower_case(pptr_check_exit);
+		if (strcmp(ptr_check_exit, "exit")==0)
 		{
-			pstr[i] = init[i];
+			ex = 0;
 		}
-
-
-		convert_case(pstr);
-
-
-		if (strcmp(pstr, "EXIT")==0 || strcmp(pstr, "eXIT")==0)
+		else
 		{
-			exiting = 0;
-		}
-		else if (exiting)
-		{
-			printf("%s\n", pstr);
-		}
-		free(pstr);
+			char* ptr_init = (char*) malloc(sizeof(char) * lenof_init);
+			strcpy(ptr_init, init);
+			char** pptr_init = &ptr_init;
 
+			convert_case(pptr_init, lenof_init);
+
+			free(ptr_init);
+		};
 	};
-
 	return 0;
 }
