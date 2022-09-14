9720b0421b2669a884806badd8e071b372f95d54
2020073190 <chdragon01@hanyang.ac.kr>
2022-09-14 20:43:03 +0900

    3-1 all done

---
+++ 3-1/3-1-1/point.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+int xpos;
+int ypos;
+}Point;
+
+Point getScale2xPoint(Point* p, int s)
+{
+	printf("Calling getScale2xPoint() for (%d, %d)\n", (*p).xpos, (*p).ypos);
+	(*p).xpos *= s;
+	(*p).ypos *= s;
+	return (*p);
+}
+
+void swapPoints(Point* p1, Point* p2)
+{
+	printf("Calling swapPoints()\n");
+	Point temp = (*p1);
+	(*p1) = (*p2);
+	(*p2) = temp;
+}
+
+int main(void)
+{
+	// initialize
+	Point p1; Point p2; 
+	Point* p_p1; Point* p_p2;
+	int p1_1; int p1_2; int p1_3;
+	int p2_1; int p2_2; int p2_3;
+
+	scanf("%d %d %d", &p1_1, &p1_2, &p1_3);
+	scanf("%d %d %d", &p2_1, &p2_2, &p2_3);
+
+	// substitute values into Point struct
+	p1.xpos = p1_1; p1.ypos = p1_2;
+	p2.xpos = p2_1; p2.ypos = p2_2;
+	p_p1 = &p1; p_p2 = &p2;
+
+
+	// terminate functions
+	getScale2xPoint(p_p1, p1_3); getScale2xPoint(p_p2, p2_3);
+	swapPoints(p_p1, p_p2);
+
+	// printing domain
+	printf("p1 : %d %d\n", p1.xpos, p1.ypos);
+	printf("p2 : %d %d\n", p2.xpos, p2.ypos);
+
+	return 0;
+}
---
+++ 3-1/3-1-2/point.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct
+{
+int xpos;
+int ypos;
+}Point;
+
+int getDistance(Point p_1, Point p_2)
+{
+    return sqrt(pow(p_1.xpos - p_2.xpos, 2) + pow(p_1.ypos - p_2.ypos, 2));
+}
+
+void getShortest(const Point* points, int n, Point* minP)
+{
+    
+    int min = getDistance(points[0], points[1]);
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            int k = getDistance(points[i], points[j]);
+            if (k <= min)
+            {
+                min = k;
+                minP[0] = points[i]; minP[1] = points[j];
+            }
+        }
+    }
+    
+    printf("shortest: (%d, %d) (%d, %d)\n", minP[0].xpos, minP[0].ypos, minP[1].xpos, minP[1].ypos);
+}
+
+void getLongest(const Point* points, int n, Point* maxP)
+{
+    
+    int max = getDistance(points[0], points[1]);
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            int k = getDistance(points[i], points[j]);
+            if (k >= max)
+            {
+                max = k;
+                maxP[0] = points[i]; maxP[1] = points[j];
+            }
+        }
+    }
+    
+    printf("longest: (%d, %d) (%d, %d)\n", maxP[0].xpos, maxP[0].ypos, maxP[1].xpos, maxP[1].ypos);
+}
+
+int main(void)
+{
+    // initialize
+    int n;
+    scanf("%d", &n);
+    Point* plist = (Point*) malloc(sizeof(Point) * n);
+    Point* min = (Point*) malloc(sizeof(Point) * 2);
+    Point* max = (Point*) malloc(sizeof(Point) * 2);
+
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d %d", &plist[i].xpos, &plist[i].ypos);
+    }
+    
+    getShortest(plist, n, min);
+    getLongest(plist, n, max);
+
+    free(plist);
+    free(min); free(max);
+}
